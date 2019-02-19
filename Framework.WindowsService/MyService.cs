7ef6d9f36bc91b6ab43227c793e8904cb834b5f8
Swaraj <ghoshswaraj2k@gmail.com>
2019-02-19 20:39:08 +0530

    Adding basic topshelf project

---
+++ Framework.WindowsService/MyService.cs
@@ -0,0 +1,15 @@
+﻿namespace Framework.WindowsService
+{
+    public class MyService
+    {
+        public void OnStart()
+        {
+            
+        }
+
+        public void OnStop()
+        {
+            
+        }
+    }
+}
---
+++ Framework.WindowsService/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace Framework.WindowsService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HostFactory.Run(hostConfigurator =>
+            {
+                hostConfigurator.SetServiceName("MyService");
+                hostConfigurator.SetDisplayName("My Service");
+                hostConfigurator.SetDescription("Does custom logic.");
+
+                hostConfigurator.RunAsLocalSystem();
+
+                hostConfigurator.Service<MyService>(serviceConfigurator =>
+                {
+                    serviceConfigurator.ConstructUsing(() => new MyService());
+
+                    serviceConfigurator.WhenStarted(service => service.OnStart());
+                    serviceConfigurator.WhenStopped(service => service.OnStop());
+                });
+            });
+        }
+    }
+}
