cdea1e5f4cddca5aec7ebb1a7b6ffb97bc97ce1e
Swaraj <ghoshswaraj2k@gmail.com>
2019-02-19 21:19:38 +0530

    Added autofac ioc

--- Framework.WindowsService/Program.cs
+++ Framework.WindowsService/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Autofac;
 using log4net.Config;
 using Topshelf;
 
@@ -14,6 +15,14 @@ namespace Framework.WindowsService
         {
             XmlConfigurator.Configure();
 
+            ContainerBuilder containerBuilder= new ContainerBuilder();
+
+            containerBuilder.RegisterType<MyService>()
+                .AsSelf()
+                .InstancePerLifetimeScope();
+
+            IContainer container = containerBuilder.Build();
+
             HostFactory.Run(hostConfigurator =>
             {
                 hostConfigurator.SetServiceName("MyService");
@@ -21,10 +30,11 @@ namespace Framework.WindowsService
                 hostConfigurator.SetDescription("Does custom logic.");
 
                 hostConfigurator.RunAsLocalSystem();
+                hostConfigurator.UseLog4Net();
 
                 hostConfigurator.Service<MyService>(serviceConfigurator =>
                 {
-                    serviceConfigurator.ConstructUsing(() => new MyService());
+                    serviceConfigurator.ConstructUsing(hostsetting => container.Resolve<MyService>());
 
                     serviceConfigurator.WhenStarted(service => service.OnStart());
                     serviceConfigurator.WhenStopped(service => service.OnStop());
